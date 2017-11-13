ff0922023418d3bb6c267e016e1a5fbf034fe693
Randyll Bearer <rlb97@pitt.edu>
2017-11-13 16:37:11 -0500

    added fields to Pairwise

--- Pairwise.java
+++ Pairwise.java
@@ -1,21 +1,26 @@
-
+import java.util.ArrayList;
 
 
 /*
 *
 */
 public class Pairwise{
-	
-	
+	ArrayList<Integer> toDisplay = new ArrayList<Integer>();	//Will hold all the rows we need to display
+	ArrayList<String> parameters = new ArrayList<String>();		//Will hold all the user-passed in parameters
+	int first = 0;
+	int second = 0;
+	int numParameters = 0;		//How many passed in user arguments there are
+	int numRows = 0;			//How many total rows there are: #param^2
+	String[][] table = null;	//Will hold the exhaustive truth table, padded to 10 character length
 	
 	/*
 	*
 	*/
 	public static void main(String[] args){
-		//Parse Inputs
+		//Parse Inputs, concatenate if > 10
 		
 		
-		//Build Exhaustive Truth Table
+		//Build Exhaustive Truth Table, #param^2 by #param
 		
 		
 		//Find all toExecute Tests
