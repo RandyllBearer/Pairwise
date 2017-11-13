5c85e4aa2a24419cfd6c63301bcabf262fa71d05
Randyll Bearer <rlb97@pitt.edu>
2017-11-13 16:30:17 -0500

    barebones

---
+++ Pairwise.java
@@ -0,0 +1,27 @@
+
+
+
+/*
+*
+*/
+public class Pairwise{
+	
+	
+	
+	/*
+	*
+	*/
+	public static void main(String[] args){
+		//Parse Inputs
+		
+		
+		//Build Exhaustive Truth Table
+		
+		
+		//Find all toExecute Tests
+		
+		
+		//Display toExecute
+		
+	}	
+}
