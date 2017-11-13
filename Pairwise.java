7069256f45540afcf3a96aa2da021ee4c75af3cd
Randyll Bearer <rlb97@pitt.edu>
2017-11-13 17:59:33 -0500

    Incorporated all methods

--- Pairwise.java
+++ Pairwise.java
@@ -1,24 +1,43 @@
 import java.util.ArrayList;
-import java.lang.Math.*;
+
+
 /*
 *
 */
 public class Pairwise{
-	ArrayList<Integer> toDisplay = new ArrayList<Integer>();	//Will hold all the rows we need to display
-	ArrayList<String> parameters = new ArrayList<String>();		//Will hold all the user-passed in parameters
-	int first = 0;
-	int second = 0;
-	int numParameters = 0;		//How many passed in user arguments there are
-	int numRows = 0;			//How many total rows there are: #param^2
-	String[][] table = null;	//Will hold the exhaustive truth table, padded to 10 character length
+	static ArrayList<Integer> toDisplay = new ArrayList<Integer>();	//Will hold all the rows we need to display
+	static ArrayList<String> parameters = new ArrayList<String>();		//Will hold all the user-passed in parameters
+	static int first = 0;
+	static int second = 0;
+	static int numParameters = 0;		//How many passed in user arguments there are
+	static int numRows = 0;			//How many total rows there are: #param^2
+	static int[][] table = null;	//Will hold the exhaustive truth table, padded to 10 character length
 	
 	/*
 	*
 	*/
+	public static void parseInput(String[] arguments){
+		if(arguments.length < 2){
+			System.out.println("ERROR: Pairwise.java requires at least two parameter arguments to produce a pairwise combination");
+			System.exit(1);
+		}else{
+			numParameters = arguments.length;
+			int i = 0;
+			while(i < numParameters){
+				if(arguments[i].length() > 10){
+					arguments[i] = arguments[i].substring(0,11);
+				}
+				parameters.add(arguments[i]);
+				i = i + 1;
+			}
+		}
+	}
 	
-	
-	public static int[][] buildOverallTruthTable(String[] cats){
-		double w = cats.length;
+	/*
+	*
+	*/
+	public static int[][] buildOverallTruthTable(){
+		double w = numParameters
 		double l = Math.pow(2, w);
 		
 		int[][] table = new int[(int)(l)][(int)(w)];
@@ -60,29 +79,149 @@ public class Pairwise{
 		
 		return table;
 	}
-
-
+	
+	/*
+	*
+	*/
+	public static void findToDisplay(){
+		
+		first = 0;	//index of parameters<>
+		second = 1;	//index of parameters<>
+		while(first < numParameters){
+			
+			boolean ff = false;
+			boolean ft = false;
+			boolean tf = false;
+			boolean tt = false;
+			int row = 0;	//which row we're on in table
+			if(first == 0 && second == 1){
+				while(!ff && !ft && !tf && !tt){
+					if(row >= numRows){
+						System.out.println("ERROR: Could not find pair coverage");
+						System.exit(2);
+					}
+					
+					//Exhaustive Checking
+					if(table[row][first] == false && table[row][second] == false && ff == false){
+						ff = true;
+						toDisplay.add(row);
+					
+					}else if(table[row][first] == false && table[row][second] == true && ft == false){
+						ft = true;
+						toDisplay.add(row);
+						
+					}else if(table[row][first] == true && table[row][second] == false && tf == false){
+						tf = true;
+						toDisplay.add(row);
+						
+					}else if(table[row][first] == true && table[row][second] == true && tt == false){
+						tt = true;
+						toDisplay.add(row);
+						
+					}
+					
+					row = row + 1;
+				}
+				
+			}else{
+				int flag = 0;
+				while(!ff && !ft && !tf && !tt){
+					if(row > numRows){
+						System.out.println("ERROR: Could not find pair coverage");
+						System.exit(2);
+					}
+					
+					if(flag == 0){
+						//Check toDisplay First
+						int i = 0;
+						while(i < toDisplay.size()){
+							if(table[i][first] == false && table[i][second] == false && ff == false){
+								ff = true;
+							
+							}else if(table[i][first] == false && table[i][second] == true && ft == false){
+								ft = true;
+								
+							}else if(table[i][first] == true && table[i][second] == false && tf == false){
+								tf = true;
+								
+							}else if(table[i][first] == true && table[i][second] == true && tt == false){
+								tt = true;
+								
+							}
+							
+							i = i + 1;
+						}
+						
+						flag = 1;
+						
+					}else{
+						//Exhaustive Checking
+						if(table[row][first] == false && table[row][second] == false && ff == false){
+							ff = true;
+							toDisplay.add(row);
+						
+						}else if(table[row][first] == false && table[row][second] == true && ft == false){
+							ft = true;
+							toDisplay.add(row);
+							
+						}else if(table[row][first] == true && table[row][second] == false && tf == false){
+							tf = true;
+							toDisplay.add(row);
+							
+						}else if(table[row][first] == true && table[row][second] == true && tt == false){
+							tt = true;
+							toDisplay.add(row);
+							
+						}
+						
+						row = row + 1;
+						
+					}
+					
+				}
+					
+			}
+			
+			//update the indices we are comparing
+			if(second == numParameters){
+				first = first + 1;
+				second = first + 1;
+			}else{
+				second = second + 1;
+			}
+		
+		}
+	}
+		
+	/*
+	*
+	*/
+	public static void displayAll(){
+		int i = 0;
+		while(i < toDisplay.size()){
+			System.out.println(toDisplay[i]);
+			
+			i = i + 1;
+		}
+	}
+	
+	/*
+	*
+	*/
 	public static void main(String[] args){
 		//Parse Inputs, concatenate if > 10
-		if(args.length < 2){
-			System.out.println("ERROR: Pairwise.java requires at least two parameter arguments to produce a pairwise combination");
-			System.exit(1);
-		}else{
-			/* int i = 0;
-			while(i < args.length){
-				if(args[i].length() > 10){
-					args[i] = args[i].substring(0,11);
-				}
-			} */
-		}
+		parseInput(args);
 		
 		//Build Exhaustive Truth Table, #param^2 by #param
-		buildOverallTruthTable(args);
+		table = buildOverallTruthTable();
 		
-		//Find all toExecute Tests
+		//Find all toDisplay Tests
+		findToDisplay();
 		
+		//Display all toDisplay
+		displayAll();
 		
-		//Display toExecute
-		
+		//Exit Main
+		System.exit(0);
 	}	
 }
